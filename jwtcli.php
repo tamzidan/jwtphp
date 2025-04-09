91b690198de2e43c6a617b26702bd715fe78e04d
tamzidan <tamzidan01@gmail.com>
2025-04-09 07:58:21 +0700

    handle generate command

--- jwtcli.php
+++ jwtcli.php
@@ -1,29 +1,53 @@
 <?php
+if ($argv[1] == 'generate') {
+    echo 'Generating JWT';
+    $secretKey = $argv[2];
+    $payload = $argv[3];
+    $header = [
+        'alg' => 'HS256',
+        'typ' => 'JWT'
+        ];
+    $headerJSON = json_encode($header);
+    $headerBase64 = rtrim(strtr(base64_encode($headerJSON), '+/', '-_'), '=');
+    $payloadBase64 = rtrim(strtr(base64_encode($payload), '+/', '-_'), '=');
+    $payloadToSign = $headerBase64 . "." . $payloadBase64;
+    $signature = hash_hmac('sha256', $payloadToSign, $secretKey);
 
-$header = [
-    'alg' => 'HS256',
-    'typ' => 'JWT'
-    ];
-$payload = [
-    'iss' => 'example.com',
-    'sub' => 'user123',
-    'iat' => time(),
-    'exp' => time() + 3600,
-    'role' => 'admin'
-    ];
-$secretKey = "123";
+    $jwt = $payloadToSign . "." . $signature;
 
-$headerJSON = json_encode($header);
-$headerBase64 = rtrim(strtr(base64_encode($headerJSON), '+/', '-_'), '=');
-$payloadJSON = json_encode($payload);
-$payloadBase64 = rtrim(strtr(base64_encode($payloadJSON), '+/', '-_'), '=');
-$payloadToSign = $headerBase64 . "." . $payloadBase64;
-$signature = hash_hmac('sha256', $payloadToSign, $secretKey);
+    var_dump($jwt);
+}
+// var_dump($argv);
 
-$jwt = $payloadToSign . "." . $signature;
+// $header = [
+//     'alg' => 'HS256',
+//     'typ' => 'JWT'
+//     ];
+// $payload = [
+//     'iss' => 'example.com',
+//     'sub' => 'user123',
+//     'iat' => time(),
+//     'exp' => time() + 3600,
+//     'role' => 'admin'
+//     ];
+// $secretKey = "123";
 
-var_dump($jwt);
-// var_dump($signature);
+// $headerJSON = json_encode($header);
+// $headerBase64 = rtrim(strtr(base64_encode($headerJSON), '+/', '-_'), '=');
+// $payloadJSON = json_encode($payload);
+// $payloadBase64 = rtrim(strtr(base64_encode($payloadJSON), '+/', '-_'), '=');
+// $payloadToSign = $headerBase64 . "." . $payloadBase64;
+// $signature = hash_hmac('sha256', $payloadToSign, $secretKey);
 
-// var_dump($headerJSON);
-// echo $payloadJSON;
+// $jwt = $payloadToSign . "." . $signature;
+
+// $jwtparts = explode('.', $jwt);
+
+// $decodedheader = json_decode(base64_decode($jwtparts[0]));
+// $decodedpayload = json_decode(base64_decode($jwtparts[1]));
+
+// $actsignature = hash_hmac('sha256', $jwtparts[0] . '.' . $jwtparts[1], $secretKey);
+
+// if ($actsignature == $signature) {
+//     echo 'Jwt Valid';
+// }
